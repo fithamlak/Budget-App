c65d65dcb3b9bb3be676d23178fe375fd4c82b50
fithamlak <justiceofgod01@gmail.com>
2023-06-13 13:34:36 +0300

    check test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe User, type: :model do
   it 'is valid with a name' do
-    user = User.create!(name: 'Justice', email: "#{Faker::Name.first_name}@try.com", password: 'password',
-                      password_confirmation: 'password')
+    user = User.new(name: 'Justice', email: "#{Faker::Name.first_name}@try.com", password: 'password',
+                    password_confirmation: 'password')
     expect(user).to be_valid
   end
 
   it 'is not valid without a name' do
-    user = User.create!(name: '', email: "#{Faker::Name.first_name}@try.com", password: 'password',
-                      password_confirmation: 'password')
+    user = User.new(name: '', email: "#{Faker::Name.first_name}@try.com", password: 'password',
+                    password_confirmation: 'password')
     expect(user).to_not be_valid
   end
 end
