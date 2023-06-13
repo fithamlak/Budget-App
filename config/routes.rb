e393e6e6d560a4477561986e3f6ac3ba9902e7c8
fithamlak <justiceofgod01@gmail.com>
2023-06-13 19:30:25 +0300

    page for expenses

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :splash, only: [:index]
   root 'splash#index'
   devise_for :users
-  resources :categories, only: [:index, :new, :create]
+  resources :categories, only: [:index, :new, :create] do
+    resources :expenses, only: [:index]
+  end
 end
