1457684c2360a9b67b66b5253e7cc25fd40c4477
fithamlak <justiceofgod01@gmail.com>
2023-06-13 19:44:17 +0300

    enable expense to be added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'splash#index'
   devise_for :users
   resources :categories, only: [:index, :new, :create] do
-    resources :expenses, only: [:index]
+    resources :expenses, only: [:index, :new, :create]
   end
 end
