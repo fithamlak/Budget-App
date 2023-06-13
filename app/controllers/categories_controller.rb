13e12630e1caf30f799c5ee40ce30c0306ec784b
fithamlak <justiceofgod01@gmail.com>
2023-06-13 19:23:56 +0300

    add home page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,26 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def
+    def index
+        @categories = Category.all
+      end
+    
+      def new
+        @category = Category.new
+      end
+    
+      def create
+        @category = Category.new(category_params)
+        if @category.save
+          redirect_to categories_path, notice: 'Category created successfully'
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+    
+      private
+    
+      def category_params
+        params.require(:category).permit(:name, :icon)
+      end
+  end
 end
