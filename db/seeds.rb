21aa8282a0b0ff196dd92874db94cc8e65d719c9
fithamlak <justiceofgod01@gmail.com>
2023-06-14 19:32:03 +0300

    Finish Final touch by Testing all

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,19 @@ user = User.create(
 )
 
 Category.destroy_all
-category = Category.create!(name: 'Utilities', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdeE_Awvws-1NPzEBfiULdhVInQFtmxfdgXV8BseWQjlLb49gkCLIsZFNlYbjDg0WrqGY&usqp=CAU')
+category = Category.create!(name: 'Utilities', icon: 'Internet')
 5.times do
   expense = Expense.new(name: Faker::Company.name, amount: rand(0.5..10.0), author: user, category_ids: [category.id])
   expense.save!
 end
 
-category = Category.create!(name: 'Books', icon: 'https://i.etsystatic.com/16421349/r/il/99a0b1/3585638490/il_794xN.3585638490_tigl.jpg')
+category = Category.create!(name: 'Books', icon: 'books')
 5.times do
   expense = Expense.new(name: Faker::Book.title, amount: rand(0.5..10.0), author: user, category_ids: [category.id])
   expense.save!
 end
 
-category = Category.create!(name: 'Movies', icon: 'https://cdn.digitbin.com/wp-content/uploads/Top-best-Android-Apps-to-watch-and-stream-free-movies-online.jpg')
+category = Category.create!(name: 'Movies', icon: 'movies')
 5.times do
   expense = Expense.new(name: Faker::Movie.title, amount: rand(0.5..10.0), author: user, category_ids: [category.id])
   expense.save!
