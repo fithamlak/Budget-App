4a5b9dd860c81fb552b1ea8403f72829ed216c48
fithamlak <justiceofgod01@gmail.com>
2023-06-14 14:23:19 +0300

    improve design as it is in the orginal

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 User.destroy_all
 user = User.create(
   name: 'Justice',
@@ -15,20 +7,20 @@ user = User.create(
 )
 
 Category.destroy_all
-category = Category.create!(name: 'House Rent', icon: 'house')
-10.times do
-  expense = Expense.new(name: Faker::Address.street_address, amount: rand(0.5..10.0), author: user, category_ids: [category.id])
+category = Category.create!(name: 'Utilities', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdeE_Awvws-1NPzEBfiULdhVInQFtmxfdgXV8BseWQjlLb49gkCLIsZFNlYbjDg0WrqGY&usqp=CAU')
+5.times do
+  expense = Expense.new(name: Faker::Company.name, amount: rand(0.5..10.0), author: user, category_ids: [category.id])
   expense.save!
 end
 
-category = Category.create!(name: 'Books', icon: 'books')
-10.times do
+category = Category.create!(name: 'Books', icon: 'https://i.etsystatic.com/16421349/r/il/99a0b1/3585638490/il_794xN.3585638490_tigl.jpg')
+5.times do
   expense = Expense.new(name: Faker::Book.title, amount: rand(0.5..10.0), author: user, category_ids: [category.id])
   expense.save!
 end
 
-category = Category.create!(name: 'Movies', icon: 'movies')
-10.times do
+category = Category.create!(name: 'Movies', icon: 'https://cdn.digitbin.com/wp-content/uploads/Top-best-Android-Apps-to-watch-and-stream-free-movies-online.jpg')
+5.times do
   expense = Expense.new(name: Faker::Movie.title, amount: rand(0.5..10.0), author: user, category_ids: [category.id])
   expense.save!
 end
