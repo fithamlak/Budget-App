d59fe41e1e08b134e98b1d36e0e823cdf5006353
fithamlak <justiceofgod01@gmail.com>
2023-06-15 04:07:45 +0300

    import @hotwires/stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import { Application } from "@hotwired/stimulus"
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// Additional code for initializing Stimulus goes here
